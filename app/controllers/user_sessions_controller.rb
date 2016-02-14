2a79afa69ab35953a9a13a0df88c98f5e13923c9
那須翔太朗 <shotaro@nasushoutarou-no-MacBook-Pro.local>
2016-02-14 17:04:31 +0900

    [add]add css for flash message

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,20 +5,21 @@ class UserSessionsController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     # TODO: 遷移先をタイムライン画面に変更する
     if @user = login(params[:email], params[:password], params[:remember])
-      redirect_back_or_to users_path, notice: 'Login successful'
+      redirect_back_or_to users_path, success: 'Login successful'
     else
-      flash.now[:alert] = 'Login failed'
+      flash.now[:danger] = 'Login failed'
       render new_user_session_path
     end
   end
 
   def destroy
     logout
-    redirect_to root_url, notice: 'Logged out!'
+    redirect_to root_url, info: 'Logged out!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'user_sessions#index'
-  resources :user_sessions
+  resources :user_sessions, only: [:index, :new, :create, :destroy]
   resources :users
   resources :dreams, only: :index
 end
